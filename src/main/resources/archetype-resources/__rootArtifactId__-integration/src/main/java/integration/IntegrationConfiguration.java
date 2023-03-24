71dc0a8c3638edfd09fbfeca240c469900f4fece
catxu <catxu@foxmail.com>
2023-03-24 09:32:18 +0800

    Introducing integration layer to decouple domain models

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-integration/src/main/java/integration/IntegrationConfiguration.java
@@ -0,0 +1,14 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.integration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author catxu
+ */
+@Configuration
+public class IntegrationConfiguration {
+}
