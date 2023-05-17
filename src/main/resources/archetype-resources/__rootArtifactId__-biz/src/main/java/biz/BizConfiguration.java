ec24c9d166468666393e3ef286f47863788a2656
catxu <catxu@foxmail.com>
2023-05-17 10:50:16 +0800

    Modified the configuration class package name to be consistent with the module name

--- src/main/resources/archetype-resources/__rootArtifactId__-biz/src/main/java/biz/BizConfiguration.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-biz/src/main/java/biz/BizConfiguration.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.service;
+package ${package}.biz;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
